e6e54777e1c1ed1a580afc9c3019df6811a8eaaf
MạnhG <khacmanh.info@gmail.com>
2021-10-14 04:29:37 +0700

    update 15/10

--- cmds/covid.js
+++ cmds/covid.js
@@ -1,8 +1,8 @@
 var __importDefault =
-  (this && this.__importDefault) ||
-  function(mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+    (this && this.__importDefault) ||
+    function(mod) {
+        return mod && mod.__esModule ? mod : { default: mod };
+    };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.location = exports.threadAdminRequired = exports.adminRequired = exports.name = void 0;
 const axios_1 = __importDefault(require("axios"));
@@ -12,51 +12,68 @@ exports.adminRequired = false;
 exports.threadAdminRequired = false;
 exports.location = __filename;
 async function default_1({ event, api }) {
-  //@procoder mew
-  var { data } = await axios.get(
-    "https://meewmeew.info/covid?apikey=Meew_xJd3Z9L5wH9dqxk9CpjFjt1ckvBBsK"
-  );
-  if (data.success == false) return api.sendMessage(data.error, event.threadID);
-  var world = data.world,
-    vn = data.vietnam,
-    news = data.news,
-    nhiemtg = world.cases,
-    chettg = world.deaths,
-    hoiphuctg = world.recovered,
-    nhiemvn = vn.cases,
-    chetvn = vn.deaths,
-    hoiphucvn = vn.recovered,
-    dieutrivn = vn.recovering,
-    ptchetvn = Math.round(
-      (chetvn.replace(/\./g, "") * 100) / nhiemvn.replace(/\./g, "")
-    ),
-    pthoiphucvn = Math.round(
-      (hoiphucvn.replace(/\./g, "") * 100) / nhiemvn.replace(/\./g, "")
-    ),
-    ptchettg = Math.round(
-      (chettg.replace(/\./g, "") * 100) / nhiemtg.replace(/\./g, "")
-    ),
-    pthoiphuctg = Math.round(
-      (hoiphuctg.replace(/\./g, "") * 100) / nhiemtg.replace(/\./g, "")
-    ),
-    pthoiphucvn = pthoiphucvn.toString().split(".")[0],
-    ptdieutrivn = (100 - pthoiphucvn - ptchetvn).toString().split(".")[0];
-  ptchetvn = ptchetvn.toString().split(".")[0];
-  pthoiphuctg = pthoiphuctg.toString().split(".")[0];
-  ptchettg = ptchettg.toString().split(".")[0];
-  return api.sendMessage(
-    "====== Thế Giới ======\n" +
-      `😷 Nhiễm: ${nhiemtg}\n` +
-      `💚 Hồi phục: ${hoiphuctg} (${pthoiphuctg}%)\n` +
-      `💀 Tử vong: ${chettg} (${ptchettg}%)\n` +
-      "====== Việt Nam ======\n" +
-      `😷 Nhiễm: ${nhiemvn}\n` +
-      `💉 Đang điều trị: ${dieutrivn} (${ptdieutrivn}%)\n` +
-      `💚 Hồi phục: ${hoiphucvn} (${pthoiphucvn}%)\n` +
-      `💀 Tử vong: ${chetvn} (${ptchetvn}%)\n\n` +
-      `Tin tức: ${news.vietnam}\n` +
-      `Cập nhật: ${data.time}`,
-    event.threadID, event.messageID
-  );
+    const moment = require("moment-timezone");
+    var time = moment.tz("Asia/Ho_Chi_Minh").format("YYYY");
+
+    let fetchdata = await axios.get("https://static.pipezero.com/covid/data.json");
+    var jsondata = (await fetchdata.data).total;
+    var vn = (await fetchdata.data).overview[6];
+    //var vn = jsondata.internal || {};
+    //var tg = jsondata.world || {};
+    var year = vn.date + '-' + time;
+
+
+
+    var world = jsondata.world,
+        //vn = vn.vietnam,
+        //news = data.news,
+        nhiemtg = world.cases,
+        chettg = world.death,
+        hoiphuctg = world.recovered,
+
+        nhiemvn = vn.cases,
+        chetvn = vn.death,
+        hoiphucvn = vn.recovered,
+        dieutrivn = vn.treating,
+
+        nhiemvn7days = vn.avgCases7day,
+        hoiphucvn7days = vn.avgRecovered7day,
+        chetvn7days = vn.avgDeath7day,
+
+
+        ptchetvn = Math.round(
+            (chetvn * 100) / nhiemvn
+        ),
+        pthoiphucvn = Math.round(
+            (hoiphucvn * 100) / nhiemvn
+        ),
+        ptchettg = Math.round(
+            (chettg * 100) / nhiemtg
+        ),
+        pthoiphuctg = Math.round(
+            (hoiphuctg * 100) / nhiemtg
+        ),
+        pthoiphucvn = pthoiphucvn.toString().split(".")[0],
+        ptdieutrivn = (100 - pthoiphucvn - ptchetvn).toString().split(".")[0];
+    ptchetvn = ptchetvn.toString().split(".")[0];
+    pthoiphuctg = pthoiphuctg.toString().split(".")[0];
+    ptchettg = ptchettg.toString().split(".")[0];
+    return api.sendMessage(
+        "====== Thế Giới ======\n" +
+        `😷 Nhiễm: ${nhiemtg}\n` +
+        `💚 Hồi phục: ${hoiphuctg} (${pthoiphuctg}%)\n` +
+        `💀 Tử vong: ${chettg} (${ptchettg}%)\n` +
+        "====== Việt Nam ======\n" +
+        `😷 Nhiễm: ${nhiemvn}\n` +
+        `💉 Đang điều trị: ${dieutrivn} (${ptdieutrivn}%)\n` +
+        `💚 Hồi phục: ${hoiphucvn} (${pthoiphucvn}%)\n` +
+        `💀 Tử vong: ${chetvn} (${ptchetvn}%)\n` +
+        `🤨 Nhiễm 7 ngày: ${nhiemvn7days}\n` +
+        `❤ Hồi phục 7 ngày:: ${hoiphucvn7days}\n` +
+        `☠️ Tử vong 7 ngày: ${chetvn7days}\n\n` +
+        //`Tin tức: ${news.vietnam}\n` +
+        `Cập nhật: ${year}`,
+        event.threadID, event.messageID
+    );
 }
 exports.default = default_1;
--- cmds/todo.js
+++ cmds/todo.js
@@ -6,6 +6,7 @@ exports.name = 'todo';
 exports.adminRequired = false;
 exports.threadAdminRequired = false;
 exports.location = __filename;
+
 function default_1({ event, botData, api }) {
     if (event.args[0] == 'del') {
         event.args = event.args.slice(1);
@@ -14,8 +15,8 @@ function default_1({ event, botData, api }) {
                 return api.sendMessage('Quyền lồn biên giới.', event.threadID, event.messageID);
             botData.todolist = [];
             api.sendMessage('Đã xóa toàn bộ việc cần làm.', event.threadID, event.messageID);
-        }
-        else {
+        } else {
+
             if (isNaN(event.args[0]))
                 return api.sendMessage('Hãy nhập số thứ tự của công việc cần xóa.', event.threadID, event.messageID);
             var todoContent = botData.todolist[parseInt(event.args[0]) - 1];
@@ -24,16 +25,15 @@ function default_1({ event, botData, api }) {
             botData.todolist.splice(botData.todolist.indexOf(todoContent), 1);
             api.sendMessage(`Đã xóa: ${todoContent}.`, event.threadID, event.messageID);
         }
-    }
-    else if (event.args[0] == 'list') {
+    } else if (event.args[0] == 'list') {
         if (botData.todolist.length == 0)
             return api.sendMessage('Không có việc cần làm.', event.threadID, event.messageID);
-        let num = 0, allWorks = '';
+        let num = 0,
+            allWorks = '';
         for (let todo of botData.todolist)
             allWorks += `\n${num += 1}. ${todo}.`;
         return api.sendMessage(`Todo List‎‎‎${allWorks}`, event.threadID, event.messageID);
-    }
-    else if (event.args[0] == 'edit') {
+    } else if (event.args[0] == 'edit') {
         event.args = event.args.slice(1);
         if (event.args.length < 2)
             return api.sendMessage('Không đúng format.', event.threadID, event.messageID);
@@ -44,15 +44,13 @@ function default_1({ event, botData, api }) {
         let todoContent = event.args.slice(1).join(' ');
         botData.todolist[parseInt(event.args[0]) - 1] = todoContent;
         api.sendMessage(`Đã đổi thành: ${todoContent}.`, event.threadID, event.messageID);
-    }
-    else if (event.type == 'message_reply') {
+    } else if (event.type == 'message_reply') {
         var todo = event.messageReply.body;
         if (!todo)
             return api.sendMessage('Chưa thấy công việc cần làm.', event.threadID, event.messageID);
         botData.todolist.push(todo);
         api.sendMessage(`Đã thêm: ${todo}.`, event.threadID, event.messageID);
-    }
-    else {
+    } else {
         let work = event.contentMsg;
         if (!work)
             return api.sendMessage('Chưa nhập công việc cần làm.', event.threadID, event.messageID);
